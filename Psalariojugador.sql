51e023a3c981136b6adfd7301df564748a9f7c86
Windar Lobo <wlobogarces@danielcastelao.org>
2023-02-02 14:45:33 +0100

    actaulizacion

---
+++ Psalariojugador.sql
@@ -0,0 +1,17 @@
+create or replace procedure p1_salarioxogador(codigo varchar)
+language PLPGSQL
+AS
+$$
+DECLARE
+s integer;
+d varchar;
+begin
+select nomx,salario into STRICT d,s from xogador where codx=codigo;
+raise notice 'o salario e o nome do xogador de codigo % é %,%',codigo,s,d;
+exception
+when no_data_found then
+raise notice 'xogador inexistente';
+
+end;
+$$
+;
