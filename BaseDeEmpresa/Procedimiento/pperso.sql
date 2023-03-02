5255c23ffb356817aad5860daccba1ba73655a84
WindaLobo <wlobogarces@danielcastelao.org.com>
2023-03-02 14:45:53 +0100

    Proyetco Factory

--- BaseDeEmpresa/Procedimiento/pperso.sql
+++ BaseDeEmpresa/Procedimiento/pperso.sql
@@ -1,9 +1,10 @@
 ﻿/*
+hay quelanzar la tabla primero de ejecutar (OjO)
 ppersoa
 procedemento chamdo ppersoa que amose para unha persoa cuxo dni se pasa por parámetro o seu nome e os postos para os que se lle fixeron entrevistas e o nome e apelidos do xestor que llas fixo 
 
 
-execute ppersoa(36555555);
+call ppersoa('36555555');
 
 persoa: elisa,bermudez,bastos
 postos e xestor que o xestionou:
@@ -25,12 +26,15 @@ DECLARE
 BEGIN
    x = '';
    select num_persoa, nom_persoa, ap1_persoa, ap2_persoa into z from persoas where dni=codigo;
-   x = x || E'\n' || z.nom_persoa || ', ' || z.ap1_persoa || ', ' || z.ap2_persoa || E'\nPostos e xestor que o xestionou:';
    
+   x = x || E'\n Persoa:' || z.nom_persoa || ', ' || z.ap1_persoa || ', ' || z.ap2_persoa || E'\n Postos e xestor que o xestionou:';
+  
    for y in select cod_posto from entrevista where num_persoa=z.num_persoa LOOP
    	select cod_xestor into w from postos where cod_posto=y.cod_posto;   
 	select nom_xestor, ap1_xestor, ap2_xestor into t from xestores where cod_xestor=w.cod_xestor;
+	
 	x = x || E'\n\t' || y.cod_posto || ' : ' || t.nom_xestor || ', ' || t.ap1_xestor || ', ' || t.ap2_xestor;
+	
    end LOOP;
    raise notice '%',x;
 
