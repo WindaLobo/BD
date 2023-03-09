487205fdedb0b723b9eeb21e22add47282cf98f1
WindaLobo <wlobogarces@danielcastelao.org.com>
2023-03-09 14:47:36 +0100

    actualizacion

--- Farmacias/trigger/impedirFarmacia.sql
+++ Farmacias/trigger/impedirFarmacia.sql
@@ -1,3 +1,5 @@
+-- insert into traballan values ('10','3688');
+
 DROP FUNCTION impedirFarmacias() CASCADE;
 CREATE FUNCTION impedirFarmacias()
 	RETURNS TRIGGER
@@ -9,14 +11,15 @@ contaP integer;
 contaF integer;
 BEGIN
  select count (*) into contaP from traballan where  codf=new.codf;
-select count (*) into contaF from farmacias  where  codf=new.codf;
- if  contaP =0 and  contaF=0  then
- raise notice ' aceptada insercion ';
+
+ if  contaP >=2 then
+ raise exception 'rechazada insecion';
+ 
     else
-    raise exception 'rechazada insecion';
+    raise notice ' aceptada insercion ';
   	 
   end if;
     return new;
 END;
 $$;
-CREATE TRIGGER impedirFarmaciast before INSERT ON farmacias for each row EXECUTE PROCEDURE impedirFarmacias();
+CREATE TRIGGER impedirFarmaciast before INSERT ON traballan for each row EXECUTE PROCEDURE impedirFarmacias();
